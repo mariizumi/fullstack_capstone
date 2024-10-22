7c96ef35b370be0cc13cc82ec44653abff1538cd
mariizumi <mariizumi6450@gmail.com>
2024-10-22 14:02:33 -0400

    Added dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -23,17 +23,18 @@ def get_request(endpoint, **kwargs):
     try:
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-request_url = sentiment_analyzer_url + "analyze/" + text
-try:
-    response = requests.get(request_url)
-    return response.json()
-except Exception as err:
-    print(f"Unexpected {err=}, {type(err)=}")
-    print("Network exception occurred")
+    request_url = sentiment_analyzer_url + "analyze/" + text
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
 
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
@@ -41,5 +42,6 @@ def post_review(data_dict):
         response = requests.post(request_url, json = data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -92,14 +92,14 @@ def get_dealerships(request, state = "All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers" + state
+        endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
     return JsonResponse({ "status": 200, "dealers": dealerships })
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
     if (dealer_id):
-        endpoint = "/fetchReviews/dealer" + str(dealer_id)
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
